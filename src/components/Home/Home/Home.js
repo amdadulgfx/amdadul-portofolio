9941721ee9bf21da40c594f63b869f1673509eb2
Md Amdadul Haq Arif <arifweb24@gmail.com>
2023-08-22 10:49:20 +0600

    responsive issue fixed

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Projects from '../../Projects/Projects';
 import './Home.css';
@@ -16,16 +16,15 @@ const Home = () => {
     }, []);
     return (
         <Container>
-            <div style={{ height: '220px', justifyContent: 'space-between', display: 'flex', color: 'white', marginTop: '80px', marginLeft: '100px', marginRight: '100px' }}>
-
-                <div className='d-flex'>
+            <Row style={{ justifyContent: 'space-between', color: 'white', marginTop: '80px', marginLeft: '100px', marginRight: '100px', }} className='d-flex flex-column-reverse flex-md-row' >
+                <Col className='d-flex  mt-3 mt-sm-0' xs={12} md={6}>
                     <div data-aos="fade-up" data-aos-duration="1000" className='title-border'></div>
                     <div>
-                        <div data-aos="fade-left" data-aos-duration="1000">
+                        <div data-aos="fade-left" data-aos-duration="1000" className=''>
                             <h1 className='fs-1 fw-bold'>I'm <br /> MD Amdadul Haq Arif</h1>
                         </div>
 
-                        <h3>
+                        <h3 style={{ height: '60px' }} >
                             <Typewriter
                                 options={{
                                     strings: ['Full Stack Developer', 'JavaScript Developer', 'I Love Coding'],
@@ -37,10 +36,12 @@ const Home = () => {
 
                         <Link to='/contactMe'><button className='regular-btn' >Hire Me</button></Link>
                     </div>
-                </div>
-                <img className='' style={{ borderRadius: '400px', border: '2px solid yellow', height: '200px', }} src={myPic} alt="" />
-            </div>
-            <div className='text-white' style={{ color: 'white', marginLeft: '100px', marginRight: '100px', }}>
+                </Col>
+                <Col className='d-flex justify-content-center align-items-center' xs={12} md={6}>
+                    <img style={{ borderRadius: '400px', border: '2px solid yellow', height: '200px', }} src={myPic} alt="" />
+                </Col>
+            </Row>
+            <div className='text-white' style={{ color: 'white', marginLeft: '100px', marginRight: '100px', marginTop: '50px', }}>
                 <h2>SKILLS</h2>
                 <div className='technology d-flex flex-wrap '>
                     <span>JavaScript(ES6)</span>
